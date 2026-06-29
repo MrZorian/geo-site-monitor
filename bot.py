baeecec558ecd5801fd9a0f00366b68e8688f190
MrZorian <malikbillalsonu@gmail.com>
2026-06-29 21:05:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ class WebsiteMonitor:
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--window-size=1920,1080)
+        chrome_options.add_argument('--window-size=1920,1080')
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_argument('--single-process')
         chrome_options.add_argument('--no-zygote')
